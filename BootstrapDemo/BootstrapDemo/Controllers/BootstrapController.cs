243c9b5351e9ca716700357cd16ddc5fc6dac2c6
deltabravo777 <97050711+deltabravo777@users.noreply.github.com>
2022-01-18 09:34:05 -0800

    Add files via upload

---
+++ BootstrapDemo/BootstrapDemo/Controllers/BootstrapController.cs
@@ -0,0 +1,266 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootstrapDemo.Controllers
+{
+    public class BootstrapController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Part03()
+        {
+            return View("Part03 - Bootstrap Grid System");
+        }
+
+        public IActionResult Part04()
+        {
+            return View("Part04 - Bootstrap 3 Grid Classes");
+        }
+
+        public IActionResult Part05()
+        {
+            return View("Part05 - Bootstrap Grid Column Offset");
+        }
+
+        public IActionResult Part06()
+        {
+            return View("Part06 - Bootstrap Nested Rows and Columns");
+        }
+
+        public IActionResult Part07()
+        {
+            return View("Part07 - Bootstrap Image Gallary");
+        }
+
+        public IActionResult Part08()
+        {
+            return View("Part08 - Bootstrap 3 Responsive Utility Classes");
+        }
+
+        public IActionResult Part09()
+        {
+            return View("Part09 - Bootstrap Typography");
+        }
+
+        public IActionResult Part10()
+        {
+            return View("Part10 - Bootstrap Paragraphs");
+        }
+
+        public IActionResult Part11()
+        {
+            return View("Part11 - Bootstrap Blockquotes and Lists");
+        }
+
+        public IActionResult Part12()
+        {
+            return View("Part12 - Bootstrap List Groups");
+        }
+
+        public IActionResult Part13()
+        {
+            return View("Part13 - Bootstrap Code Blocks");
+        }
+
+        public IActionResult Part14()
+        {
+            return View("Part14 - Bootstrap Table Classes");
+        }
+
+        public IActionResult Part15()
+        {
+            return View("Part15 - Bootstrap Button Classes");
+        }
+
+        public IActionResult Part16()
+        {
+            return View("Part16 - Bootstrap Icons");
+        }
+
+        public IActionResult Part17()
+        {
+            return View("Part17 - Bootstrap Dropdown");
+        }
+
+        public IActionResult Part18()
+        {
+            return View("Part18 - Bootstrap Button Group");
+        }
+
+        public IActionResult Part19()
+        {
+            return View("Part19 - Bootstrap Split Button Dropdown");
+        }
+
+        public IActionResult Part20()
+        {
+            return View("Part20 - Bootstrap Forms");
+        }
+
+        public IActionResult Part21()
+        {
+            return View("Part21 - Bootstrap Form Controls");
+        }
+
+        public IActionResult Part22()
+        {
+            return View("Part22 - Bootstrap Disabled and Readonly Form Controls");
+        }
+
+        public IActionResult Part23()
+        {
+            return View("Part23 - Bootstrap Form Validation States");
+        }
+
+        public IActionResult Part24()
+        {
+            return View("Part24 - Bootstrap Form Controls Height and Width");
+        }
+
+        public IActionResult Part25()
+        {
+            return View("Part25 - Bootstrap Input Groups");
+        }
+
+        public IActionResult Part26()
+        {
+            return View("Part26 - Bootstrap Button in Input Groups");
+        }
+
+        public IActionResult Part27()
+        {
+            return View("Part27 - Bootstrap nav Component");
+        }
+
+        public IActionResult Part28()
+        {
+            return View("Part28 - Bootstrap navbar component");
+        }
+
+        public IActionResult Part29()
+        {
+            return View("Part29 - Bootstrap Breadcrumbs");
+        }
+
+        public IActionResult Part30()
+        {
+            return View("Part30 - Bootstrap Pager and Pagination");
+        }
+
+        public IActionResult Part31()
+        {
+            return View("Part31 - Bootstrap Panels");
+        }
+
+        public IActionResult Part32()
+        {
+            return View("Part32 - Bootstrap Well");
+        }
+
+        public IActionResult Part33()
+        {
+            return View("Part33 - Bootstrap Labels and Badges");
+        }
+
+        public IActionResult Part34()
+        {
+            return View("Part34 - Bootstrap Progress Bars");
+        }
+
+        public IActionResult Part35()
+        {
+            return View("Part35 - Bootstrap Jumbotron");
+        }
+
+        public IActionResult Part36()
+        {
+            return View("Part36 - Bootstrap Media Object");
+        }
+
+        public IActionResult Part37()
+        {
+            return View("Part37 - Bootstrap Collapse Plugin");
+        }
+
+        public IActionResult Part38()
+        {
+            return View("Part38 - Bootstrap Accordion");
+        }
+
+        public IActionResult Part39()
+        {
+            return View("Part39 - Bootstrap Accordion with Arrows");
+        }
+
+        public IActionResult Part40()
+        {
+            return View("Part40 - Bootstrap Modal Popup");
+        }
+
+        public IActionResult Part41()
+        {
+            return View("Part41 - Bootstrap Modal Methods and Events");
+        }
+
+        public IActionResult Part42()
+        {
+            return View("Part42 - Bootstrap Tabs Plugin");
+        }
+
+        public IActionResult Part43()
+        {
+            return View("Part43 - Bootstrap Tooltip");
+        }
+
+        public IActionResult Part44()
+        {
+            return View("Part44 - Bootstrap Tooltip Manual Trigger");
+        }
+
+        public IActionResult Part45()
+        {
+            return View("Part45 - Bootstrap Popover");
+        }
+
+        public IActionResult Part46()
+        {
+            return View("Part46 - Bootstrap Alert");
+        }
+
+        public IActionResult Part47()
+        {
+            return View("Part47 - Bootstrap Scrollspy");
+        }
+
+        public IActionResult Part48()
+        {
+            return View("Part48 - Bootstrap Scrollspy Not Working");
+        }
+
+        public IActionResult Part49()
+        {
+            return View("Part49 - Bootstrap Scrollspy Vertical Menu");
+        }
+
+        public IActionResult Part50()
+        {
+            return View("Part50 - Bootstrap Affix Plugin");
+        }
+
+        public IActionResult Part51()
+        {
+            return View("Part51 - Bootstrap Image Carousel");
+        }
+
+        public IActionResult Part52()
+        {
+            return View("Part52 - Bootstrap Multi Column Carousel");
+        }
+    }
+}
